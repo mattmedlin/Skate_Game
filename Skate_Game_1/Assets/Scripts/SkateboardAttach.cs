61384dc13725c7a5ecf88791da970d403f7db26e
mattmedlin <50458786+mattmedlin@users.noreply.github.com>
2020-05-27 17:25:20 -0500

    attached skateboard

---
+++ Skate_Game_1/Assets/Scripts/SkateboardAttach.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkateboardAttach : MonoBehaviour
+{
+    public Transform skateboard;
+    public Transform target;
+    public float DstFromTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position - transform.up * DstFromTarget;
+        transform.rotation = target.rotation;
+    }
+}
